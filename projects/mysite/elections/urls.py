a2290cc308940f545e258347d4eae329959ff038
heechulJung <heechul.dev@gmail.com>
2021-08-11 11:43:57 +0900

    투표 화면

--- projects/mysite/elections/urls.py
+++ projects/mysite/elections/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$', views.index),
-   
-    
+    url(r'^$', views.index),
+    url(r'^areas/(?P<area>.+)/$',views.areas)
+    # url(r'^areas/(?P<area>\d)/$',views.areas) --숫자만 허용
 ]
