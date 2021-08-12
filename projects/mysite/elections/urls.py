b0d0c5b0e51438125673fe8dc3665718300eff2a
heechulJung <heechul.dev@gmail.com>
2021-08-12 17:12:07 +0900

    선거

--- projects/mysite/elections/urls.py
+++ projects/mysite/elections/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^areas/(?P<area>.+)/$',views.areas)
+    url(r'^areas/(?P<area>.+)/$',views.areas),
+    url(r'^polls/(?P<poll_id>\d+)/$', views.polls)
     # url(r'^areas/(?P<area>\d)/$',views.areas) --숫자만 허용
 ]
