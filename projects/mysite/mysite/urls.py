5d1acc0367614bce8dae72b97a9b8701d04fab2a
heechulJung <heechul.dev@gmail.com>
2021-08-17 16:34:48 +0900

    선거

--- projects/mysite/mysite/urls.py
+++ projects/mysite/mysite/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.static import serve
 
 urlpatterns = [
     url('', include('elections.urls')),
     url('admin/', admin.site.urls),
-    
+    url(r'^static/(?P<path>.*)', serve, kwargs={'insecure': True})
 ]
